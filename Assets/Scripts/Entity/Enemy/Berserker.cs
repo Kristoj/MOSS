92448b26c38bd365f54ef66e6d10ee536faed1ee
Kristoj <johansson.kristo@gmail.com>
2019-06-28 13:21:28 +0300

    loma

---
+++ Assets/Scripts/Entity/Enemy/Berserker.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Berserker : MonoBehaviour {
+
+    [SerializeField] private float pollRate = 5f;
+    [SerializeField] private bool canPoll = true;
+
+    private NavMeshAgent agent;
+
+    void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Start() {
+        StartCoroutine(PollAction());
+    }
+
+    IEnumerator PollAction() {
+        while (canPoll) {
+            // Update agent destination
+            agent.SetDestination(GameManager.LocalPlayer.transform.position);
+            yield return new WaitForSeconds(1 / pollRate);
+        }
+    }
+}
